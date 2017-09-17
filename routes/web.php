a56673bc2c0e6a58be20c9cdb80d01e8f02587d5
jProgr <8537691@gmail.com>
2017-09-17 11:11:13 -0500

    Add follows page per user

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/{username}/follows', 'UsersController@follows');
 Route::get('/{username}', 'UsersController@show');
